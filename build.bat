c4b10650761228ae17879e024928322b09cbe44d
Peter Okruhlica <peterokruhlica@gmail.com>
2025-11-24 18:05:17 +0100

    Update built.bat

--- build.bat
+++ build.bat
@@ -15,6 +15,21 @@ call .venv\Scripts\activate
 
 echo.
 echo ==========================================
+echo Step 1.5: Check dependencies
+echo ==========================================
+
+
+pip show pyinstaller >nul 2>&1
+
+if %errorlevel% neq 0 (
+    echo PyInstaller not found. Installing now...
+    pip install pyinstaller
+) else (
+    echo PyInstaller is already installed. Skipping installation.
+)
+
+echo.
+echo ==========================================
 echo Step 2: find version
 echo ==========================================
 
@@ -24,10 +39,10 @@ echo Found version: %VERSION%
 echo.
 
 echo ==========================================
-echo Step 3: buld .exe
+echo Step 3: build .exe
 echo ==========================================
 
-pyinstaller --onefile --clean --name "GridAgent_v%VERSION%" main.py
+pyinstaller --onefile --noconsole --clean --name "GridAgent_v%VERSION%" main.py
 
 echo.
 echo ==========================================
